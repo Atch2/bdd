035dc637c515765f68d04aa90d6aa9a9bd7e8a6a
Anthony <tidsec_inf@outlook.com>
2023-09-20 07:42:02 -0500

    second commit

--- crear_videojuegos.sql
+++ crear_videojuegos.sql
@@ -1,7 +1,7 @@
 create table videojuegos(
-	codigo_videojuego int not null,
+	codigo int not null,
 	nombre varchar(100) not null,
 	descripcion varchar(300),
 	valoracion int not null,
-	constraint codigo_videojuego_pk primary key(codigo_videojuego)
+	constraint codigo_videojuego_pk primary key(codigo)
 )
