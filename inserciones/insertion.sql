91aa69483de4be7963c8ff55915fc67cd8324ba6
Anthony <tidsec_inf@outlook.com>
2023-09-21 07:55:53 -0500

    add directory inserciones and selecciones

---
+++ inserciones/insertion.sql
@@ -0,0 +1,180 @@
+-----productos
+insert into productos(codigo, nombre, descripcion, precio, stock)
+values(1, 'Anthony', 'djafkhakfaf', 3.50, 4)
+
+insert into productos(codigo, nombre, descripcion, precio, stock)
+values(2, 'Fabian', 'djafkhakfaf', 9.50, 5)
+
+insert into productos(codigo, nombre, descripcion, precio, stock)
+values(3, 'Mateo', 'djafkhakfaf', 8.50, 6)
+
+insert into productos(codigo, nombre, descripcion, precio, stock)
+values(4, 'Debora', 'djafkhakfaf', 3.50, 7)
+
+insert into productos(codigo, nombre, descripcion, precio, stock)
+values(5, 'Paola', 'djafkhakfaf', 13.50, 8)
+
+insert into productos(codigo, nombre, precio, stock)
+values(6, 'Anthony', 4.50, 9)
+
+insert into productos(codigo, nombre, precio, stock)
+values(7, 'Patricio', 6.50, 10)
+
+insert into productos(codigo, nombre, precio, stock)
+values(8, 'Dolas', 56.50, 11)
+
+----cuentas
+insert into cuentas(numero_cuenta, cedula_propietario, fecha_creacion, saldo)
+values (12345, 12345, '19/09/2015', 1.50);
+
+insert into cuentas(numero_cuenta, cedula_propietario, fecha_creacion, saldo)
+values (22355, 12345, '19/09/2015', 1.50);
+
+insert into cuentas(numero_cuenta, cedula_propietario, fecha_creacion, saldo)
+values (32345, 12345, '19/09/2015', 1.50);
+
+insert into cuentas(numero_cuenta, cedula_propietario, fecha_creacion, saldo)
+values (42345, 12345, '19/09/2015', 1.50);
+
+insert into cuentas(numero_cuenta, cedula_propietario, fecha_creacion, saldo)
+values (52345, 12345, '19/09/2015', 1.50);
+
+insert into cuentas(numero_cuenta, cedula_propietario, fecha_creacion, saldo)
+values (62345, 12345, '19/09/2015', 1.50);
+
+insert into cuentas(numero_cuenta, cedula_propietario, fecha_creacion, saldo)
+values (72345, 12345, '19/09/2015', 1.50);
+
+insert into cuentas(numero_cuenta, cedula_propietario, fecha_creacion, saldo)
+values (82345, 12345, '19/09/2015', 1.50);
+
+insert into cuentas(numero_cuenta, cedula_propietario, fecha_creacion, saldo)
+values (92345, 12345, '19/09/2015', 1.50);
+
+insert into cuentas(numero_cuenta, cedula_propietario, fecha_creacion, saldo)
+values (10245, 12345, '19/09/2015', 1.50);
+------ estudiantes
+insert into estudiantes(cedula, nombre, apellido, email, fecha_nacimiento)
+values('1234567891', 'Anthony', 'Chan', 'and@gmail.com', '12/03/2002');
+
+insert into estudiantes(cedula, nombre, apellido, email, fecha_nacimiento)
+values('1234567892', 'Anthony', 'Chan', 'and@gmail.com', '12/03/2002');
+
+insert into estudiantes(cedula, nombre, apellido, email, fecha_nacimiento)
+values('1234567893', 'Anthony', 'Chan', 'and@gmail.com', '12/03/2002');
+
+insert into estudiantes(cedula, nombre, apellido, email, fecha_nacimiento)
+values('1234567894', 'Anthony', 'Chan', 'and@gmail.com', '12/03/2002');
+
+insert into estudiantes(cedula, nombre, apellido, email, fecha_nacimiento)
+values('1234567895', 'Anthony', 'Chan', 'and@gmail.com', '12/03/2002');
+
+insert into estudiantes(cedula, nombre, apellido, email, fecha_nacimiento)
+values('1234567896', 'Anthony', 'Chan', 'and@gmail.com', '12/03/2002');
+
+insert into estudiantes(cedula, nombre, apellido, email, fecha_nacimiento)
+values('1234567897', 'Anthony', 'Chan', 'and@gmail.com', '12/03/2002');
+
+insert into estudiantes(cedula, nombre, apellido, email, fecha_nacimiento)
+values('1234567898', 'Anthony', 'Chan', 'and@gmail.com', '12/03/2002');
+--registro_entrada
+insert into registros_entrada(codigo_registro, cedula_empleado, fecha, hora)
+values(1, '1234567891', '17/09/2019', '15:00');
+
+insert into registros_entrada(codigo_registro, cedula_empleado, fecha, hora)
+values(2, '1234567891', '17/09/2019', '15:00');
+
+insert into registros_entrada(codigo_registro, cedula_empleado, fecha, hora)
+values(3, '1234567891', '17/09/2019', '15:00');
+
+insert into registros_entrada(codigo_registro, cedula_empleado, fecha, hora)
+values(4, '1234567891', '17/09/2019', '15:00');
+
+insert into registros_entrada(codigo_registro, cedula_empleado, fecha, hora)
+values(5, '1234567891', '17/09/2019', '15:00');
+
+insert into registros_entrada(codigo_registro, cedula_empleado, fecha, hora)
+values(6, '1234567891', '17/09/2019', '15:00');
+
+insert into registros_entrada(codigo_registro, cedula_empleado, fecha, hora)
+values(7, '1234567891', '17/09/2019', '15:00');
+
+insert into registros_entrada(codigo_registro, cedula_empleado, fecha, hora)
+values(8, '1234567891', '17/09/2019', '15:00');
+
+insert into registros_entrada(codigo_registro, cedula_empleado, fecha, hora)
+values(9, '1234567891', '17/09/2019', '15:00');
+
+insert into registros_entrada(codigo_registro, cedula_empleado, fecha, hora)
+values(10, '1234567891', '17/09/2019', '15:00');
+
+------videojuegos
+insert into videojuegos(codigo_videojuego, nombre, descripcion, valoracion)
+values(1, 'Anthony', 'dkafdlakfal', 100);
+
+insert into videojuegos(codigo_videojuego, nombre, descripcion, valoracion)
+values(2, 'Anthony', 'dkafdlakfal', 100);
+
+insert into videojuegos(codigo_videojuego, nombre, descripcion, valoracion)
+values(3, 'Anthony', 'dkafdlakfal', 100);
+
+insert into videojuegos(codigo_videojuego, nombre, descripcion, valoracion)
+values(4, 'Anthony', 'dkafdlakfal', 100);
+
+insert into videojuegos(codigo_videojuego, nombre, descripcion, valoracion)
+values(5, 'Anthony', 'dkafdlakfal', 100);
+
+insert into videojuegos(codigo_videojuego, nombre, valoracion)
+values(6, 'Anthony', 100);
+
+insert into videojuegos(codigo_videojuego, nombre, valoracion)
+values(7, 'Anthony', 100);
+
+insert into videojuegos(codigo_videojuego, nombre, valoracion)
+values(8, 'Anthony', 100);
+
+-----transacciones
+insert into transacciones(codigo, numero_cuenta, monto, tipo, fecha, hora)
+values(1, 12345, 1.50, 'D', '19/09/2020', '14:00');
+
+insert into transacciones(codigo, numero_cuenta, monto, tipo, fecha, hora)
+values(2, 12345, 1.50, 'D', '19/09/2020', '14:00');
+
+insert into transacciones(codigo, numero_cuenta, monto, tipo, fecha, hora)
+values(3, 12345, 1.50, 'D', '19/09/2020', '14:00');
+
+insert into transacciones(codigo, numero_cuenta, monto, tipo, fecha, hora)
+values(4, 12345, 1.50, 'D', '19/09/2020', '14:00');
+
+insert into transacciones(codigo, numero_cuenta, monto, tipo, fecha, hora)
+values(5, 12345, 1.50, 'D', '19/09/2020', '14:00');
+
+insert into transacciones(codigo, numero_cuenta, monto, tipo, fecha, hora)
+values(6, 12345, 1.50, 'D', '19/09/2020', '14:00');
+
+insert into transacciones(codigo, numero_cuenta, monto, tipo, fecha, hora)
+values(7, 12345, 1.50, 'D', '19/09/2020', '14:00');
+
+insert into transacciones(codigo, numero_cuenta, monto, tipo, fecha, hora)
+values(8, 12345, 1.50, 'D', '19/09/2020', '14:00');
+
+insert into transacciones(codigo, numero_cuenta, monto, tipo, fecha, hora)
+values(9, 12345, 1.50, 'D', '19/09/2020', '14:00');
+
+insert into transacciones(codigo, numero_cuenta, monto, tipo, fecha, hora)
+values(10, 12345, 1.50, 'D', '19/09/2020', '14:00');
+
+----- personas ----
+insert into personas(cedula, nombre, apellido)
+values ('1754426034', 'Anthony', 'Changotasig')
+
+insert into personas(cedula, nombre, apellido, estatura)
+values ('1754426036', 'Fabian', 'Changotasig', 1.85)
+
+insert into personas(cedula, nombre, apellido, estatura, fecha_nacimiento, hora_nacimiento, cantidad_ahorrada, numero_hijos)
+values('1754426087', 'Mateo', 'Sanchez', 1.80, '12/02/2023', '23:00', 300.25, 3)
+
+
+select  cedula, nombre, numero_hijos from personas
+select * from personas
+
